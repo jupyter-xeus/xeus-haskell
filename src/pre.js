d9305487aafaf6a9df83cf60f5e5316c91626a96
Johan Mabille <johan.mabille@gmail.com>
2026-03-30 17:01:25 +0200

    Fixed lite build

--- src/pre.js
+++ src/pre.js
@@ -1,55 +1,9 @@
-const installLegacyWasmFsMountShim = () => {
-  if (!Module?.FS || typeof Module.FS.mount !== "function") {
-    return;
-  }
-  if (Module.FS.__xhLegacyWasmFsMountShimInstalled) {
-    return;
-  }
+Module.preRun = Module.preRun || [];
 
-  const originalMount = Module.FS.mount.bind(Module.FS);
-
-  Module.FS.mount = (type, opts, mountpoint) => {
-    if (!type || typeof type.createBackend === "function") {
-      return originalMount(type, opts, mountpoint);
-    }
-
-    // jupyterlite-xeus can still pass legacy FS mount objects.
-    if (typeof _wasmfs_create_memory_backend !== "function") {
-      console.warn("[xeus-haskell] WasmFS shim unavailable, using original mount");
-      return originalMount(type, opts, mountpoint);
-    }
-
-    const typeName = type.name || type.constructor?.name || "legacy-fs-type";
-    console.warn(
-      `[xeus-haskell] WasmFS shim: ${typeName} -> memory backend at ${mountpoint}`
-    );
-    return originalMount(
-      { createBackend: () => _wasmfs_create_memory_backend() },
-      opts,
-      mountpoint
-    );
-  };
-
-  Module.FS.__xhLegacyWasmFsMountShimInstalled = true;
-};
-
-const previousPreRun = Module.preRun;
-Module.preRun = () => {
-  if (Array.isArray(previousPreRun)) {
-    previousPreRun.forEach((fn) => typeof fn === "function" && fn());
-  } else if (typeof previousPreRun === "function") {
-    previousPreRun();
-  }
-
+Module.preRun.push(function() {
   ENV.MHSDIR = "/share/microhs";
   ENV.MHS_LIBRARY_PATH = "/usr/lib/haskell-packages/microhs";
-  installLegacyWasmFsMountShim();
-};
+  Module.ENV.MHSDIR = ENV.MHSDIR;
+  Module.ENV.MHS_LIBRARY_PATH = ENV.MHS_LIBRARY_PATH;
+});
 
-const previousRuntimeInitialized = Module.onRuntimeInitialized;
-Module.onRuntimeInitialized = () => {
-  installLegacyWasmFsMountShim();
-  if (typeof previousRuntimeInitialized === "function") {
-    previousRuntimeInitialized();
-  }
-};
